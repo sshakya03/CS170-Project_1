3510aae87f56e8b17d41d972c04cdaf6862e48c1
sshakya03 <github-registered-email>
2025-01-29 17:57:50 -0800

    created nPuzzle

---
+++ nPuzzle.py
@@ -0,0 +1,19 @@
+import heapq as min_heap_esque_queue
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def main():
+    puzzle_mode = input("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own." + '\n')
+
+    if puzzle_mode == "1"
+    
+    return
+
+if __name__ == "__main__":
+    main()
+
+
